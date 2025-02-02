04e2fbe129176546c4ce2e3579068ee593af84f8
sandesh <sandeshbhatta218@gmail.com>
2025-02-02 13:27:28 +0545

    All files copied to app folders and develeopment is started.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Dimensions, View } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ backgroundColor: 'dodgerblue', width: 100, height: 50, top: 20 }} />
+
+      <View style={{ backgroundColor: 'gold', width: 100, height: 50 }} />
+
+      <View style={{ backgroundColor: 'tomato', width: 100, height: 50, top: -30 }} />
     </View>
   );
 }
@@ -13,8 +16,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 30,
+    // justifyContent: 'space-between',//main
+    alignItems: "center",//secondary
+    flexDirection: 'row',//horizontal axis
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
   },
 });
+
+
+/**
+ flex:1 = entire screen
+ flex:0.5 = half screen
+*/
